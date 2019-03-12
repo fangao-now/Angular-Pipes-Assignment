e24f170e885479a658643ffe88a078021c563eb1
coolcrazydog <fan.gao1989@gmail.com>
2019-03-11 23:47:40 -0400

    implement sort pipe (however sorting/filtering logic should happen in the component instead of in the pipe) see this discussion: https://angular.io/guide/pipes#appendix-no-filterpipe-or-orderbypipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ReversePipe } from './pipes/reverse.pipe';
+import { SortPipe } from './pipes/sort.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ReversePipe
+    ReversePipe,
+    SortPipe
   ],
   imports: [
     BrowserModule,
